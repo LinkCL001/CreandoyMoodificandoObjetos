329f853afb66e967c7fecd6338082673fb2b9d6d
JuanQuezada <ju.quezada.c@gmail.com>
2021-09-21 15:17:16 -0300

    commit

--- Assets/js/Scripts.js
+++ Assets/js/Scripts.js
@@ -1,34 +1,96 @@
-function paciente (nombre, edad, rut, diagnosico) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.rut = rut;
-        this.diagnosico = diagnosico;
+function Paciente (nombre, edad, rut, diagnostico) {
+        // this.nombre = nombre;
+        // this.edad = edad;
+        // this.rut = rut;
+        // this.diagnosico = diagnostico;
     var _nombre = nombre
-        Object.defineProperty(this, "_getpaciente", {
+        Object.defineProperty(this, "_getNombre", {
             get: function() {
                 return _nombre
             }
-        })
-        Object.defineProperty(this, "_setpaciente", {
-            set: function(nom){
-                _nombre = nom
+        });
+        Object.defineProperty(this, "_setNombre", {
+            set: function(nombre){
+                _nombre = nombre
             }
-        })        
+        }); 
+    var _edad = edad
+        Object.defineProperty(this, "_getEdad", {
+            get: function() {
+                return _edad
+            }
+        });
+        Object.defineProperty(this, "_setEdad", {
+            set: function(edad){
+                _edad = edad
+            }
+        });      
+    var _rut = rut
+        Object.defineProperty(this, "_getRut", {
+            get: function() {
+                return _rut
+            }
+        });
+        Object.defineProperty(this, "_setRut", {
+            set: function(rut){
+                _rut = rut
+            }
+        });       
+    var _diagnostico = diagnostico
+        Object.defineProperty(this, "_getDiagnostico", {
+            get: function() {
+                return _diagnostico
+            }
+        });
+        Object.defineProperty(this, "_setDiagnostico", {
+            set: function(diagnostico){
+                _diagnostico = diagnostico
+            }
+        });             
 }
 
-paciente.prototype.getpaciente = function(){
-    return this._getpaciente
-}
+Paciente.prototype.getNombre = function(){
+    return this._getNombre;
+};
 
-paciente.prototype.setpaciente = function(nom){
-    return this._setpaciente = nom
-}
+Paciente.prototype.setNombre = function(nombre){
+    this._setNombre = nombre;
+};
 
-var paciente1 = new paciente("Juan", 30, "17.103.697-6", "SBO")
+Paciente.prototype.getEdad = function(){
+    return this._getEdad;
+};
+
+Paciente.prototype.setNombre = function(edad){
+    this._setEdad = edad;
+};
+
+Paciente.prototype.getRut = function(){
+    return this._getRut;
+};
+
+Paciente.prototype.setRut = function(rut){
+    this._setRut = rut;
+};
+
+Paciente.prototype.getDiagnostico = function(){
+    return this._getDiagnostico;
+};
+
+Paciente.prototype.setDiagnostico = function(diagnostico){
+    this._setDiagnostico = diagnostico;
+};
+
+var paciente1 = new Paciente("Juan", 32, "17.103.697-6", "SBO");
 
 
-function consultorio (nombre, pacientes){
+function Consultorio (nombre, pacientes){
     this.nombre = nombre;
     this.pacientes = pacientes || [];
 }
-var consultorio1 = new consultorio("Consultorio Desafio Latam", [paciente1])
+
+Consultorio.prototype.agregar_paciente = function(_paciente){
+    this.personas.push(_paciente);
+}
+
+var consultorio1 = new Consultorio("Consultorio Desafio Latam", [paciente1])
